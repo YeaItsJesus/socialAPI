dc44fe2d7d3c6f2df78ede81b113c1d95d056a8a
YeaItsJesus <122085651+YeaItsJesus@users.noreply.github.com>
2023-06-01 15:40:35 -0500

    added getUserByID

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,3 +10,16 @@ module.exports = {
       res.status(400).json(err);
     }
   },
+  async getUserById(req, res) {
+    try {
+      const dbUserData = await User.findOne({ _id: req.params.id });
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No User Found, Check ID' });
+        return;
+      }
+      res.json(dbUserData);
+    } catch (err) {
+      console.log(err);
+      res.status(400).json(err);
+    }
+  },
